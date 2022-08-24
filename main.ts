89728d69000711d8eb78752a322389e4fcba8b7e
sololim <101944276+sololim@users.noreply.github.com>
2022-08-24 18:46:15 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (input.temperature() > 10) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+})
